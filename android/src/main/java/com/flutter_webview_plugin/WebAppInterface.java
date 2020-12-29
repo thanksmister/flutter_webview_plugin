6a273c1afb2b0d57115dd68f5f1912d8de9d8275
Michael Ritchie <mister@thanksmister.com>
2020-12-29 20:19:59 -0300

    * added handler to interface

--- android/src/main/java/com/flutter_webview_plugin/WebAppInterface.java
+++ android/src/main/java/com/flutter_webview_plugin/WebAppInterface.java
@@ -1,11 +1,15 @@
 package com.flutter_webview_plugin;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.webkit.JavascriptInterface;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 class WebAppInterface {
+
+    private Handler uiThreadHandler = new Handler(Looper.getMainLooper());
 
     /**
      * Show a toast from the web page
@@ -16,19 +20,24 @@ class WebAppInterface {
     }
 
     @JavascriptInterface
-    public void getExternalAuth(String payload) {
-        try {
-            JSONObject json = new JSONObject(payload);
-            if (json != null) {
-                boolean force = false;
-                if (json.has("force")) {
-                    force = json.getBoolean("force");
+    public void getExternalAuth(final String payload) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSONObject json = new JSONObject(payload);
+                    if (json != null) {
+                        boolean force = false;
+                        if (json.has("force")) {
+                            force = json.getBoolean("force");
+                        }
+                        FlutterWebviewPlugin.channel.invokeMethod("getExternalAuth", force);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //Toast.makeText(mContext, "Error handling external auth method.", Toast.LENGTH_SHORT).show();
                 }
-                FlutterWebviewPlugin.channel.invokeMethod("getExternalAuth", force);
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-            //Toast.makeText(mContext, "Error handling external auth method.", Toast.LENGTH_SHORT).show();
-        }
+        });
     }
 }
