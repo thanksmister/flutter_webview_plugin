3f044a589e54e2fdb90006798a32412a385a0d12
Michael Ritchie <mister@thanksmister.com>
2020-12-30 11:21:34 -0300

    * removed example

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/michaelritchie/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/michaelritchie/AndroidStudioProjects/flutter_webview_plugin"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_BUILD_NAME=0.3.11"
+export "FLUTTER_BUILD_NUMBER=0.3.11"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
